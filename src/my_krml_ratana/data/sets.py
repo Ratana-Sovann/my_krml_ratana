d1621775395d716b4ebfec2da1509e000125ff3f
Ratana-Sovann <sovann.ratana@student.uts.edu.au>
2024-09-16 21:35:41 +1000

    add savesets

--- src/my_krml_ratana/data/sets.py
+++ src/my_krml_ratana/data/sets.py
@@ -23,6 +23,45 @@ def pop_target(df, target_col):
 
     return df_copy, target
 
+def save_sets(X_train=None, y_train=None, X_val=None, y_val=None, X_test=None, y_test=None, path='../data/processed/'):
+    """Save the different sets locally
+
+    Parameters
+    ----------
+    X_train: Numpy Array
+        Features for the training set
+    y_train: Numpy Array
+        Target for the training set
+    X_val: Numpy Array
+        Features for the validation set
+    y_val: Numpy Array
+        Target for the validation set
+    X_test: Numpy Array
+        Features for the testing set
+    y_test: Numpy Array
+        Target for the testing set
+    path : str
+        Path to the folder where the sets will be saved (default: '../data/processed/')
+
+    Returns
+    -------
+    """
+    import numpy as np
+
+    if X_train is not None:
+      np.save(f'{path}X_train', X_train)
+    if X_val is not None:
+      np.save(f'{path}X_val',   X_val)
+    if X_test is not None:
+      np.save(f'{path}X_test',  X_test)
+    if y_train is not None:
+      np.save(f'{path}y_train', y_train)
+    if y_val is not None:
+      np.save(f'{path}y_val',   y_val)
+    if y_test is not None:
+      np.save(f'{path}y_test',  y_test)
+
+
 
 def load_sets(path='../data/processed/'):
     """Load the different locally save sets
@@ -85,7 +124,6 @@ def subset_x_y(target, features, start_index:int, end_index:int):
     return features[start_index:end_index], target[start_index:end_index]
 
 
-# Solution
 def split_sets_by_time(df, target_col, test_ratio=0.2):
     """Split sets by indexes for an ordered dataframe
 
